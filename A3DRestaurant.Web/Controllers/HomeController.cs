bf5e381b860b542199454d86f968553e02688d90
Dhananjay Kr Pandey <dhananjaykrpandey@outlook.com>
2020-07-09 21:06:26 +0400

    other html added

--- A3DRestaurant.Web/Controllers/HomeController.cs
+++ A3DRestaurant.Web/Controllers/HomeController.cs
@@ -22,7 +22,23 @@ namespace A3DRestaurant.Web.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
 
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
